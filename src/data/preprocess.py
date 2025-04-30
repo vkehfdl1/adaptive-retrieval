9af784825a7d4e0232dfa90c8f45622202e35c28
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2025-04-30 15:49:28 +0900

    add preprocess code for MLDR

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -10,6 +10,7 @@ from datetime import datetime
 
 import click
 import pandas as pd
+from datasets import load_dataset
 
 from src.utils.util import extract_elements
 
@@ -65,6 +66,84 @@ def preprocess_ko_strategyqa():
 	return corpus, qa_train, qa_dev
 
 
+def preprocess_mldr():
+	corpus = load_dataset("Shitao/MLDR", "corpus-ko", split="corpus").to_pandas()
+	# Preprocess Corpus
+	corpus.reset_index(drop=True, inplace=True)
+	corpus.rename(
+		columns={
+			"docid": "doc_id",
+			"text": "contents",
+		},
+		inplace=True,
+	)
+	corpus["metadata"] = corpus["doc_id"].apply(
+		lambda x: {
+			"last_modified_datetime": datetime.now(),
+			"prev_id": None,
+			"next_id": None,
+		}
+	)
+
+	train_qa = load_dataset("Shitao/MLDR", "ko", split="train").to_pandas()
+	dev_qa = load_dataset("Shitao/MLDR", "ko", split="dev").to_pandas()
+	test_qa = load_dataset("Shitao/MLDR", "ko", split="test").to_pandas()
+
+	# Need to add positive samples (or negative samples) to the corpus if there is no id.
+	def add_to_corpus(corpus_df: pd.DataFrame, qa_df: pd.DataFrame) -> pd.DataFrame:
+		qa_df["positive_doc_ids"] = qa_df["positive_passages"].apply(
+			lambda x: [elem["docid"] for elem in x]
+		)
+		qa_df["positive_doc_contents"] = qa_df["positive_passages"].apply(
+			lambda x: [elem["text"] for elem in x]
+		)
+		qa_df_exploded = qa_df.explode("positive_doc_ids", ignore_index=True).explode(
+			"positive_doc_contents", ignore_index=True
+		)
+
+		qa_df_filtered = qa_df_exploded[
+			~qa_df_exploded["positive_doc_ids"].isin(corpus_df["doc_id"])
+		]
+		qa_df_filtered.rename(
+			columns={"positive_doc_ids": "doc_id", "positive_doc_contents": "contents"},
+			inplace=True,
+		)
+		qa_df_filtered["metadata"] = qa_df_filtered["doc_id"].apply(
+			lambda x: {
+				"last_modified_datetime": datetime.now(),
+				"prev_id": None,
+				"next_id": None,
+			}
+		)
+		corpus_df = pd.concat(
+			[corpus_df, qa_df_filtered[["doc_id", "contents", "metadata"]]]
+		)
+		return corpus_df
+
+	corpus = add_to_corpus(corpus, train_qa)
+	corpus = add_to_corpus(corpus, dev_qa)
+	corpus = add_to_corpus(corpus, test_qa)
+
+	def preprocess_qa(df: pd.DataFrame):
+		df.reset_index(drop=True, inplace=True)
+
+		df.rename(columns={"query_id": "qid"}, inplace=True)
+		df["retrieval_gt"] = df["positive_passages"].apply(
+			lambda x: [[elem["docid"]] for elem in x]
+		)
+		df.drop(columns=["positive_passages", "negative_passages"], inplace=True)
+		df["generation_gt"] = df["qid"].apply(lambda x: [""])
+
+		return df[["qid", "query", "retrieval_gt", "generation_gt"]]
+
+	return (
+		corpus,
+		preprocess_qa(train_qa),
+		preprocess_qa(dev_qa),
+		preprocess_qa(test_qa),
+	)
+
+
 @click.command()
 @click.option("--save_dir", type=click.Path(file_okay=False, dir_okay=True))
 @click.option(
@@ -81,6 +160,8 @@ def main(save_dir: str, dataset_name: str):
 		corpus.to_parquet(os.path.join(save_dir, "corpus.parquet"), index=False)
 		qa_train.to_parquet(os.path.join(save_dir, "qa_train.parquet"), index=False)
 		qa_dev.to_parquet(os.path.join(save_dir, "qa_dev.parquet"), index=False)
+	elif dataset_name == "mldr":
+		corpus = preprocess_mldr()
 
 
 if __name__ == "__main__":
---
+++ tests/src/data/test_preprocess.py
@@ -0,0 +1,20 @@
+from autorag.utils import (
+	validate_corpus_dataset,
+	validate_qa_dataset,
+	validate_qa_from_corpus_dataset,
+)
+
+from src.data.preprocess import preprocess_mldr
+
+
+def test_preprocess_mldr():
+	corpus, train_qa, dev_qa, test_qa = preprocess_mldr()
+
+	validate_corpus_dataset(corpus)
+	validate_qa_dataset(train_qa)
+	validate_qa_dataset(dev_qa)
+	validate_qa_dataset(test_qa)
+
+	validate_qa_from_corpus_dataset(train_qa, corpus)
+	validate_qa_from_corpus_dataset(dev_qa, corpus)
+	validate_qa_from_corpus_dataset(test_qa, corpus)

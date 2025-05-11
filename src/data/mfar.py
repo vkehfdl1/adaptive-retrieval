fcf65cfdd540a9f4a6f70cfc2aa2217375728486
Jeffrey (Dongkyu) Kim <vkehfdl1@gmail.com>
2025-05-11 21:00:43 +0900

    use pre-retrieval result from autorag result

--- src/data/mfar.py
+++ src/data/mfar.py
@@ -1,3 +1,6 @@
+import pathlib
+from typing import Union
+
 import pandas as pd
 import pytorch_lightning as pl
 import torch
@@ -6,23 +9,43 @@ from torch.utils.data import Dataset, DataLoader, default_collate
 
 
 class AutoRAGQADataset(Dataset):
-	def __init__(self, df: pd.DataFrame):
-		assert "query" in df.columns
-		assert "query_embeddings" in df.columns
+	def __init__(
+		self,
+		qa_df: pd.DataFrame,
+		semantic_retrieval_df: pd.DataFrame,
+		lexical_retrieval_df: pd.DataFrame,
+	):
+		self.qa_df = qa_df
+		self.semantic_df = semantic_retrieval_df
+		self.lexical_df = lexical_retrieval_df
 
-		self.df = df
+		assert len(self.qa_df) == len(self.semantic_df) == len(self.lexical_df)
 
 	def __len__(self):
-		return len(self.df)
+		return len(self.qa_df)
 
 	def __getitem__(self, idx: int):
-		row = self.df.iloc[idx]
+		qa_row = self.qa_df.iloc[idx]
 		return {
-			"query": row["query"] if isinstance(row["query"], str) else row["query"][0],
+			"query": qa_row["query"]
+			if isinstance(qa_row["query"], str)
+			else qa_row["query"][0],
 			"query_embeddings": torch.from_numpy(
-				row["query_embeddings"].copy()
+				qa_row["query_embeddings"].copy()
 			).float(),
-			"retrieval_gt": to_list(row["retrieval_gt"]),
+			"retrieval_gt": to_list(qa_row["retrieval_gt"]),
+			"semantic_retrieved_ids": to_list(
+				self.semantic_df.iloc[idx]["retrieved_ids"]
+			),
+			"semantic_retrieve_scores": torch.from_numpy(
+				self.semantic_df.iloc[idx]["retrieve_scores"]
+			).float(),
+			"lexical_retrieved_ids": to_list(
+				self.lexical_df.iloc[idx]["retrieved_ids"]
+			),
+			"lexical_retrieve_scores": torch.from_numpy(
+				self.lexical_df.iloc[idx]["retrieve_scores"]
+			).float(),
 		}
 
 
@@ -36,42 +59,61 @@ class AutoRAGDataLoader(DataLoader):
 	def _collate_fn(self, batch):
 		queries = list(map(lambda x: x["query"], batch))
 		retrieval_gt_list = list(map(lambda x: x["retrieval_gt"], batch))
-		collate_query_embeddings = default_collate(
-			list(map(lambda x: x["query_embeddings"], batch))
-		)
-
-		return {
+		semantic_retrieved_ids = list(map(lambda x: x["semantic_retrieved_ids"], batch))
+		lexical_retrieved_ids = list(map(lambda x: x["lexical_retrieved_ids"], batch))
+		result = {
 			"query": queries,
 			"retrieval_gt": retrieval_gt_list,
-			"query_embeddings": collate_query_embeddings,
+			"semantic_retrieved_ids": semantic_retrieved_ids,
+			"lexical_retrieved_ids": lexical_retrieved_ids,
 		}
+
+		other_data = {}
+		for key in batch[0].keys():
+			if key not in list(result.keys()):
+				other_data[key] = default_collate([item[key] for item in batch])
+
+		result.update(other_data)
+
+		return result
 
 
 class MfarDataModule(pl.LightningDataModule):
 	def __init__(
 		self,
-		qa_train_data_path,
-		qa_test_data_path,
+		qa_train_data_path: Union[pathlib.Path, str],
+		semantic_retrieval_df_path: Union[pathlib.Path, str],
+		lexical_retrieval_df_path: Union[pathlib.Path, str],
 		batch_size: int = 32,
 		num_workers: int = 0,
 	):
 		super().__init__()
 		self.qa_train_data_path = qa_train_data_path
-		self.qa_test_data_path = qa_test_data_path
+		self.semantic_retrieval_df_path = semantic_retrieval_df_path
+		self.lexical_retrieval_df_path = lexical_retrieval_df_path
 		self.batch_size = batch_size
 		self.num_workers = num_workers
 
 	def setup(self, stage):
 		if stage == "fit":
-			df = pd.read_parquet(self.qa_train_data_path, engine="pyarrow")
-			train_df = df.sample(frac=0.85, random_state=42).reset_index(drop=True)
-			valid_df = df.drop(train_df.index).reset_index(drop=True)
-			self.train_dataset = AutoRAGQADataset(train_df)
-			self.valid_dataset = AutoRAGQADataset(valid_df)
-
-		if stage == "test":
-			df = pd.read_parquet(self.qa_test_data_path, engine="pyarrow")
-			self.test_dataset = AutoRAGQADataset(df)
+			qa_df = pd.read_parquet(self.qa_train_data_path, engine="pyarrow")
+			semantic_retrieval_df = pd.read_parquet(
+				self.semantic_retrieval_df_path, engine="pyarrow"
+			)
+			lexical_retrieval_df = pd.read_parquet(
+				self.lexical_retrieval_df_path, engine="pyarrow"
+			)
+			slice_idx = int(len(qa_df) * 0.85)
+			self.train_dataset = AutoRAGQADataset(
+				qa_df.iloc[:slice_idx].reset_index(drop=True),
+				semantic_retrieval_df.iloc[:slice_idx].reset_index(drop=True),
+				lexical_retrieval_df.iloc[:slice_idx].reset_index(drop=True),
+			)
+			self.valid_dataset = AutoRAGQADataset(
+				qa_df.iloc[slice_idx:].reset_index(drop=True),
+				semantic_retrieval_df.iloc[slice_idx:].reset_index(drop=True),
+				lexical_retrieval_df.iloc[slice_idx:].reset_index(drop=True),
+			)
 
 	def train_dataloader(self):
 		return AutoRAGDataLoader(
@@ -88,14 +130,3 @@ class MfarDataModule(pl.LightningDataModule):
 			batch_size=self.batch_size,
 			num_workers=self.num_workers,
 		)
-
-	def test_dataloader(self):
-		return AutoRAGDataLoader(
-			dataset=self.test_dataset,
-			shuffle=False,
-			batch_size=self.batch_size,
-			num_workers=self.num_workers,
-		)
-
-	def predict_dataloader(self):
-		return self.test_dataloader()
--- tests/src/data/test_mfar.py
+++ tests/src/data/test_mfar.py
@@ -1,5 +1,6 @@
 import pathlib
 
+import numpy as np
 import pandas as pd
 import torch
 
@@ -8,19 +9,40 @@ from src.data.mfar import AutoRAGQADataset, AutoRAGDataLoader
 root_dir = pathlib.Path(__file__).parent.parent.parent
 resource_dir = root_dir / "resources"
 
+semantic_result_df = pd.DataFrame(
+	{
+		"retrieved_ids": [
+			np.array(["tung", "tung1", "tung2"]),
+			np.array(["jax", "jax1", "jax2"]),
+			np.array(["ha", "ver", "tz"]),
+		],
+		"retrieve_scores": [
+			np.array([0.4, 0.5, 0.6]),
+			np.array([0.23, 0.12, 0.5]),
+			np.array([0.5, 0.2, 0.4]),
+		],
+	}
+)
+
 
 def test_autorag_dataset():
 	# Check the data loader is working
-	df = pd.read_parquet(
-		resource_dir / "qa_sample_embeddings.parquet", engine="pyarrow"
-	)
-	dataset = AutoRAGQADataset(df)
+	df = pd.read_parquet(resource_dir / "qa_sample_embeddings.parquet")
+	dataset = AutoRAGQADataset(df, semantic_result_df, semantic_result_df)
+
 	loader = AutoRAGDataLoader(dataset=dataset, batch_size=2)
 
 	for batch in loader:
 		assert "query" in batch.keys()
 		assert "retrieval_gt" in batch.keys()
 		assert "query_embeddings" in batch.keys()
+		assert "semantic_retrieved_ids" in batch.keys()
+		assert "semantic_retrieve_scores" in batch.keys()
+		assert "lexical_retrieved_ids" in batch.keys()
+		assert "lexical_retrieve_scores" in batch.keys()
 		assert len(batch["query"]) == len(batch["retrieval_gt"])
 		assert isinstance(batch["query_embeddings"], torch.Tensor)
+		assert isinstance(batch["semantic_retrieved_ids"], list)
+		assert isinstance(batch["semantic_retrieve_scores"], torch.Tensor)
 		assert batch["query_embeddings"].dim() == 2
+		assert batch["semantic_retrieve_scores"].shape[1] == 3
